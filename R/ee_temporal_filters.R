aaca76aef0f627564a8bfa03d913c2b1dc08813a
zackarno <zackarno@gmail.com>
2022-04-21 17:48:42 +0300

    add `ee_month_filter`, `ee_year_month_filter`

--- R/ee_temporal_filters.R
+++ R/ee_temporal_filters.R
@@ -18,23 +18,20 @@ ee_year_filter <- function(imageCol,...){
 
 
 ee_year_filter.ee.imagecollection.ImageCollection <-  function(imageCol,
-                                                               start_date,
-                                                               end_date,
+                                                               year,
                                                                ...){
 
-  stopifnot(!is.null(imageCol), inherits(imageCol, "ee.imagecollection.ImageCollection"))
-  if(is.numeric(start_date) & nchar(start_date)==4){
-    start_year = start_date
-    end_year = end_date
-  }else{
-    start_year = lubridate::year(start_date)
-    end_year = lubridate::year(end_date)
-  }
 
-  years = rgee::ee$List$sequence(start_year, end_year)
+  stopifnot(!is.null(imageCol), inherits(imageCol, "ee.imagecollection.ImageCollection"))
+  assertthat::assert_that(is.numeric(year)&length(year)>1,
+                          msg = "month must be a numeric vector of lenght greater than 0")
+
+  # should make assertion for no duplicates
+
+  ee_year_list <- ee$List(year) # switched from ee$List$sequence - let the user make sequence in R or suppply raw
 
   ic_list <-
-    years$map(rgee::ee_utils_pyfunc(function (y) {
+    ee_year_list$map(rgee::ee_utils_pyfunc(function (y) {
       imageCol$filter(rgee::ee$Filter$calendarRange(y, y, 'year'))
     }
 
@@ -44,5 +41,95 @@ ee_year_filter.ee.imagecollection.ImageCollection <-  function(imageCol,
 
   cat("returning  ImageCollection of x\n")
   return(rgee::ee$ImageCollection(fc_from_ic_list$flatten()))
+
+}
+
+
+#' ee_month_filter
+#'
+#' @param imageCol ee$ImageCollection
+#' @param ... other arguments
+#'
+#' @return ee$ImageCollection or ee$Image filtered by year
+#' @export
+
+ee_month_filter <- function(imageCol,month,...){
+
+  UseMethod('ee_month_filter')
+
+}
+
+
+#' @export
+
+
+ee_month_filter.ee.imagecollection.ImageCollection <-  function(imageCol,
+                                                                month,
+
+
+                                                               ...){
+
+  stopifnot(!is.null(imageCol), inherits(imageCol, "ee.imagecollection.ImageCollection"))
+  assertthat::assert_that(is.numeric(month)&length(month)>1,
+                          msg = "month must be a numeric vector of lenght greater than 0")
+  assertthat::assert_that(all(month %in% c(1:12)),
+                          msg = "month values must be integeer inside 1-12 range")
+
+  # should make assertion for no duplicates
+
+  ee_month_list <- ee$List(month) # switched from ee$List$sequence - let the user make sequence in R or suppply raw
+
+  ic_list <-
+    ee_month_list$map(rgee::ee_utils_pyfunc(function (m) {
+      imageCol$filter(rgee::ee$Filter$calendarRange(m, m, 'month'))
+    }
+
+    ))
+
+  fc_from_ic_list <- rgee::ee$FeatureCollection(ic_list)
+
+  cat("returning  ImageCollection of x\n")
+  return(rgee::ee$ImageCollection(fc_from_ic_list$flatten()))
+
+}
+
+#' ee_year_month_filter
+#'
+#' @param imageCol ee$ImageCollection
+#' @param year \code{numeric} vector contain years to filter
+#' @param month \code{numeric} vector contain months to filter
+#' @param ... other arguments
+#'
+#' @return ee$ImageCollection or ee$Image filtered by year & month
+#' @export
+
+ee_year_month_filter <- function(imageCol,year, month,...){
+
+  UseMethod('ee_year_month_filter')
+
+}
+
+
+#' @export
+
+
+ee_year_month_filter.ee.imagecollection.ImageCollection <-  function(imageCol,
+                                                                     year,
+                                                                     month,
+                                                                     ...){
+  # assertions
+  stopifnot(!is.null(imageCol), inherits(imageCol, "ee.imagecollection.ImageCollection"))
+  assertthat::assert_that(is.numeric(year)&length(year)>0,
+                          msg = "year must be a numeric vector of lenght greater than 0")
+  assertthat::assert_that(is.numeric(month)&length(month)>0,
+                          msg = "month must be a numeric vector of lenght greater than 0")
+
+  year_ic <-  ee_year_filter(imageCol = imageCol,year=year)
+  yr_mo_ic <-  ee_month_filter(imageCol=year_ic,month=month)
+  return(yr_mo_ic)
+
+
+
+
 
 }
