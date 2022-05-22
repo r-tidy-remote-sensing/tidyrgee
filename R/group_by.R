805bfce64a5cf8f07f2e04ba86e31438a25295ef
zackarno <zackarno@gmail.com>
2022-05-22 11:43:23 -0400

    update group_by.imagecollection.ImageCollection w/ `as_tidyee`

--- R/group_by.R
+++ R/group_by.R
@@ -2,19 +2,11 @@
 
 #' @export
 group_by.ee.imagecollection.ImageCollection <- function(x,...){
-
-  new_groups <- rlang::enquos(..., .ignore_empty = "all")
-
-  class(x) <- c("grouped_imageCol", class(x))
-  new_groups_list <- new_groups |> purrr::map(~rlang::quo_get_expr(.x))
-  new_groups_chr <- as.character(unlist(new_groups_list))
-
-  assertthat::assert_that(new_groups_chr %in% c("year","month") ,
-                          msg = "so far can only group by year, month, or both")
-  cat(glue::glue("returning imageCol grouped by {new_groups_chr}\n"))
-  attr(x,"grouped_vars") <- new_groups_chr
-  return(x)
-
+  stopifnot(!is.null(x), inherits(x, "ee.imagecollection.ImageCollection"))
+  convert_to_tidyee_warning()
+  x_tidy <- as_tidyee(x)
+  x_tidy |>
+    group_by(...)
 }
 
 
