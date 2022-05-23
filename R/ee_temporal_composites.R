b771502c083349942f63e7eef53f6ed1a26ad94a
zackarno <zackarno@gmail.com>
2022-05-23 08:18:03 -0400

    patch for ee_year_composite

--- R/ee_temporal_composites.R
+++ R/ee_temporal_composites.R
@@ -79,12 +79,14 @@ ee_year_composite.tidyee<-  function(x,
 
     ))
   )
+  client_bandnames<- paste0(vrt_band_names(x),"_",stat)
   vrt_summarised <- x$vrt |>
     dplyr::summarise(
       dates_summarised= list(date),
-      band_names= list(band_names), # this might be a better way than how it is for funcs below
-      groups = "drop"
-    )
+
+      .groups = "drop"
+    ) |>
+    mutate(band_names= list(client_bandnames))
   create_tidyee(ic_summarised,vrt_summarised)
 }
 
