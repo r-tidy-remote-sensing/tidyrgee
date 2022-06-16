474131389cd9eac14d114a5b7bf4b3096c63654e
zackarno <zackarno@gmail.com>
2022-06-16 17:45:06 -0400

    bind_ics changed back to pre-reticulate error bug, but for-loop replaced

--- R/bind.R
+++ R/bind.R
@@ -18,21 +18,62 @@
 #' }
 #'
 
-bind_ics <- function(x){
-
+bind_ics <- function(x,large_list=F){
   ic_only <- x |>
-    purrr::map("ee_ob")
+    purrr::map(~.x$ee_ob)
   vrt_only <- x |>
-    purrr::map("vrt")
+    purrr::map(~.x$vrt)
 
   vrt_together<- dplyr::bind_rows(vrt_only)
+  ic_container <- ee$ImageCollection(ee$List(ic_only))
+  if(!large_list)
 
-  ic_container = ee$ImageCollection(list())
-  for(i in 1:length(ic_only)){
-    ic_container=ic_container$merge(ic_only[[i]])
+  return(
+  create_tidyee(x = ic_container$sort(prop = "system:time_start"),vrt = vrt_together )
+  )
+  if(large_list){
+    return(
+    as_tidyee(ic_container)
+    )
 
   }
-  create_tidyee(x = ic_container$sort(prop = "system:time_start"),vrt = vrt_together )
+
+
+  # ic_container = ee$ImageCollection(list())
+  # for(i in 1:length(ic_only)){
+  #   ic_container=ic_container$merge(ic_only[[i]])
+  #
+  # }
+
+
+}
+
+#' bind ImageCollections only (not tidyee)
+#'
+#' @param x list of tidyee objects
+#' @return tidyee object containing single image collection and vrt
+#' @export
+#'
+#' @examples \dontrun{
+#' library(tidyrgee)
+#' library(rgee)
+#' ee_Initialize()
+#' modis_ic <- ee$ImageCollection("MODIS/006/MOD13Q1")
+#' modis_ic_tidy <- as_tidyee(modis_ic)
+#' modis_tidy_list <- modis_tidy |>
+#' group_split(month)
+#' modis_tidy_list |>
+#'   bind_ics()
+#' }
+#'
+
+bind_ics_only <- function(x){
+  ic_only <- x |>
+    purrr::map(~.x$ee_ob)
+
+  ic_container <- ee$ImageCollection(ee$List(ic_only))
+  return(ic_container)
+
 
 }
 
