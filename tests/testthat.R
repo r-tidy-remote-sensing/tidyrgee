8a57ec1cb62d7c55430632bcab3bf6c03331fefc
joshualerickson <joshualerickson@gmail.com>
2022-09-01 19:24:14 -0600

    try again

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,28 +3,8 @@ library(tidyrgee)
 library(sf)
 library(rgee)
 
-Sys.setenv(EARTHENGINE_PYTHON="/usr/bin/python3")
-Sys.setenv(RETICULATE_PYTHON="/usr/bin/python3")
 
-# Necessary Python packages were loaded?
-skip_if_no_pypkg <- function() {
-  have_ee <- reticulate::py_module_available("ee")
-  have_numpy <- reticulate::py_module_available("numpy")
-  if (isFALSE(have_ee)) {
-    skip("ee not available for testing")
-  }
-  if (isFALSE(have_numpy)) {
-    skip("numpy not available for testing")
-  }
-}
+ee_Initialize()
 
-
-# Initialize credentials
-# If you do not count with GCS credentials the test will be skipped
-have_ee <- reticulate::py_module_available("ee")
-have_numpy <- reticulate::py_module_available("numpy")
-if (have_ee & have_numpy) {
-  ee_Initialize()
-}
 
 test_check("tidyrgee")
