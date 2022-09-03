662295e0997f1adc6b8cb1e01a198a1908dbebb6
joshualerickson <joshualerickson@gmail.com>
2022-09-03 09:50:25 -0600

    just trying lw version

--- tests/testthat.R
+++ tests/testthat.R
@@ -9,12 +9,8 @@ Sys.setenv(RETICULATE_PYTHON="/usr/bin/python3")
 # Necessary Python packages were loaded?
 skip_if_no_pypkg <- function() {
   have_ee <- reticulate::py_module_available("ee")
-  have_numpy <- reticulate::py_module_available("numpy")
   if (isFALSE(have_ee)) {
     skip("ee not available for testing")
-  }
-  if (isFALSE(have_numpy)) {
-    skip("numpy not available for testing")
   }
 }
 
@@ -22,8 +18,7 @@ skip_if_no_pypkg <- function() {
 # Initialize credentials
 # If you do not count with GCS credentials the test will be skipped
 have_ee <- reticulate::py_module_available("ee")
-have_numpy <- reticulate::py_module_available("numpy")
-if (have_ee & have_numpy) {
+if (have_ee) {
   ee_Initialize()
 }
 
